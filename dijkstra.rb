c9f4e2ece2c188ad748a8e7b8a7cc21833d82e56
Lee Saville <l.w.saville@gmail.com>
2017-03-14 08:29:07 -0600

    Move things into a Dijkstra class

--- dijkstra.rb
+++ dijkstra.rb
@@ -1,5 +1,46 @@
 require 'pry'
 
+class Dijkstra
+  attr_reader :graph, :parents
+  attr_accessor :costs
+
+  def initialize(graph, costs, parents)
+    @graph   = graph
+    @costs   = costs
+    @parents = parents
+  end
+
+  def find_lowest_cost_node
+    lowest_cost = Float::INFINITY
+    lowest_cost_node = nil
+    @costs.keys.each do |node|
+      cost = @costs[node]
+      if cost < lowest_cost && !@processed.include?(node)
+        lowest_cost = cost
+        lowest_cost_node = node
+      end
+    end
+    lowest_cost_node
+  end
+
+  def walk_the_graph
+    node = find_lowest_cost_node
+    while node != 'fin'
+      cost = @costs[node]
+      neighbors = @graph[node]
+      neighbors.keys.each do |n|
+        new_cost = cost + neighbors[n]
+        if @costs[n] > new_cost
+          @costs[n] = new_cost
+          @parents[n] = node
+        end
+      end
+      @processed << node
+      node = find_lowest_cost_node
+    end
+  end
+end
+
 =begin
 @graph = {}
 @graph['start']      = {}
@@ -12,7 +53,7 @@ require 'pry'
 @graph['b']['fin']   = 5
 =end
 
-@graph = {
+graph = {
   'start' => {
     'a' => 6,
     'b' => 2
@@ -26,48 +67,18 @@ require 'pry'
   }
 }
 
-@costs = {
+costs = {
   'a' => 6,
   'b' => 2,
   'fin' => Float::INFINITY
 }
 
-@parents = {
+parents = {
   'a'   => 'start',
   'b'   => 'start',
   'fin' => ''
 }
 
-@processed = []
-
-def find_lowest_cost_node
-  lowest_cost = Float::INFINITY
-  lowest_cost_node = nil
-  @costs.keys.each do |node|
-    cost = @costs[node]
-    if cost < lowest_cost && !@processed.include?(node)
-      lowest_cost = cost
-      lowest_cost_node = node
-    end
-  end
-  lowest_cost_node
-end
-
-node = find_lowest_cost_node
-while node != 'fin'
-  cost = @costs[node]
-  neighbors = @graph[node]
-  neighbors.keys.each do |n|
-    new_cost = cost + neighbors[n]
-    if @costs[n] > new_cost
-      @costs[n] = new_cost
-      @parents[n] = node
-    end
-  end
-  @processed << node
-  node = find_lowest_cost_node
-end
-
 binding.pry
 
-blah = 2
+d = Dijkstra.new(graph, costs, parents)
