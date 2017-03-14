9e39ff5d162b8a4595f35030fc9b60e25b6fec0a
Lee Saville <l.w.saville@gmail.com>
2017-03-14 07:59:08 -0600

    Add find_lowest_cost method. Add ivars

--- dijkstra.rb
+++ dijkstra.rb
@@ -1,39 +1,55 @@
 require 'pry'
 
-graph = {}
+@graph = {}
 graph['start']      = {}
 graph['start']['a'] = 6
 graph['start']['b'] = 2
+graph['a']          = {}
 graph['a']['fin']   = 1
+graph['b']          = {}
 graph['b']['a']     = 3
 graph['b']['fin']   = 5
 
-costs = {
+@costs = {
   'a' => 6,
   'b' => 2,
-  fin => Float::INFINITY
+  'fin' => Float::INFINITY
 }
 
-parents = {
+@parents = {
   'a'   => 'start',
   'b'   => 'start',
   'fin' => ''
 }
 
-processed = []
+@processed = []
+
+def find_lowest_cost_node
+  lowest_cost = Float::INFINITY
+  lowest_cost_node = nil
+  @costs.keys.each do |node|
+    cost = @costs[node]
+    binding.pry
+    if cost < lowest_cost && !@processed.include?(node)
+      lowest_cost = cost
+      lowest_cost_node = node
+    end
+  end
+  lowest_cost_node
+end
 
 node = find_lowest_cost_node(costs)
 while node.length > 0
-  cost = costs[node]
-  neighbors = graph[node]
+  cost = @costs[node]
+  neighbors = @graph[node]
   neighbors.keys.each do |n|
     new_cost = cost + neighbors[n]
-    if costs[n] > new_cost
-      costs[n] = new_cost
-      parents[n] = node
+    if @costs[n] > new_cost
+      @costs[n] = new_cost
+      @parents[n] = node
     end
   end
-  processed << node
+  @processed << node
   node = find_lowest_cost_node(costs)
 end
 
